9d455e250d5fd2e5060b71f994c6e9078b343c32
Stock84-dev <leontk8@gmail.com>
2022-09-03 21:22:57 +0200

    browse clion project files with ranger

--- data/linux/scripts/clion_open_file.sh
+++ data/linux/scripts/clion_open_file.sh
@@ -4,4 +4,5 @@ FILE=/tmp/clion_file_pick
 
 alacritty -e ranger --choosefile=$FILE $(dirname $1)
 sleep 0.1
-xdotool key --delay 0 "colon" "e" "space" $(cat $FILE | sed 's/./& /g' | sed 's/\//slash/g' | sed 's/_/underscore/g' | sed 's/\./period/g' | sed 's/-/minus/g') "Enter"
+xdotool key --delay 1 "colon" "e" "space" 
+xdotool key --delay 0 $(cat $FILE | sed 's/./& /g' | sed 's/\//slash/g' | sed 's/_/underscore/g' | sed 's/\./period/g' | sed 's/-/minus/g') "Enter"
