7e6c24deadc1101616eacbde6edd533e9beacb05
Stock84-dev <leontk8@gmail.com>
2024-06-14 09:21:02 +0200

    update

--- data/linux/scripts/backblaze_backup.sh
+++ data/linux/scripts/backblaze_backup.sh
@@ -2,4 +2,4 @@
 
 rclone sync / BackblazeCrypt:Frankenstein-stock-pc/root --links --exclude={"/dev/*","/swapfile","/raid/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/*","/media/*","/lost+found", "/home/stock/ssd/", "/home/stock/data/", "/home/stock/nvme"}
 rclone sync /home/stock/ssd BackblazeCrypt:Frankenstein-stock-pc/ssd --links --exclude /home/stock/ssd/rustc_target/
-rclone sync /home/stock/data BackblazeCrypt:Frankenstein-stock-pc/data --links
+rclone sync /home/stock/data BackblazeCrypt:Frankenstein-stock-pc/data --links --exclude /home/stock/data/Downloads/Torrents
