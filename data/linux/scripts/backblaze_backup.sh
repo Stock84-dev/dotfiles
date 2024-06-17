d35525eaf8f532b5c3b6c809beb8b68cd8490d2d
Stock84-dev <leontk8@gmail.com>
2024-06-17 17:52:54 +0200

    update

--- data/linux/scripts/backblaze_backup.sh
+++ data/linux/scripts/backblaze_backup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-rclone sync / BackblazeCrypt:Frankenstein-stock-pc/root --links --exclude={"/dev/*","/swapfile","/raid/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/*","/media/*","/lost+found", "/home/stock/ssd/", "/home/stock/data/", "/home/stock/nvme"}
-rclone sync /home/stock/ssd BackblazeCrypt:Frankenstein-stock-pc/ssd --links --exclude /home/stock/ssd/rustc_target/
-rclone sync /home/stock/data BackblazeCrypt:Frankenstein-stock-pc/data --links --exclude /home/stock/data/Downloads/Torrents
+rclone sync / BackblazeCrypt:Frankenstein-stock-pc/root --links --exclude={"/dev/*","/swapfile","/raid/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/*","/media/*","/lost+found", "/home/stock/ssd/", "/home/stock/data/", "/home/stock/nvme"} &
+rclone sync /home/stock/ssd BackblazeCrypt:Frankenstein-stock-pc/ssd --links --exclude /home/stock/ssd/rustc_target/ &
+rclone sync /home/stock/data BackblazeCrypt:Frankenstein-stock-pc/data --links --exclude /home/stock/data/Downloads/Torrents &
